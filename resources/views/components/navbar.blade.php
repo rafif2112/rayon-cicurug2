832c6bb3bf53f6b3a64b09edd1f8f569c7da34e1
rafif2112 <m.rafif887@gmail.com>
2024-12-07 21:39:21 +0700

    update link

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,13 +4,21 @@
                 <img class="w-6 md:w-8 -mt-[2px]" src="{{asset('assets/images/icon/cicurug2.png')}}" alt="">
                 <span class="self-center whitespace-nowrap bg-gradient-to-l from-blue-400 to-blue-700 bg-clip-text text-2xl font-extrabold text-transparent md:text-3xl">Cicurug 2</span>
             </a>
-            <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden" aria-controls="navbar-dropdown" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+            <button data-collapse-toggle="navbar-dropdown" type="button" 
+                class="inline-flex h-12 w-12 items-center justify-center rounded-lg p-2 
+                text-gray-600 transition-all duration-200
+                hover:bg-blue-50 hover:text-blue-600
+                focus:outline-none focus:ring-2 focus:ring-blue-200
+                dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-600 
+                md:hidden"
+                aria-controls="navbar-dropdown" 
+                aria-expanded="false">
+                <span class="sr-only">Toggle navigation menu</span>
+                <svg class="h-6 w-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                        d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
-            </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+            </button>    <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
                 <ul class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
                     <li>
                         <a href="{{route('home')}}" class="{{ Request::is('/') ? 'text-blue-500 dark:bg-blue-600 md:text-blue-700 md:dark:bg-transparent md:dark:text-blue-500' : 'text-black hover:text-blue-700 hover:bg-gray-100 md:hover:bg-transparent' }} block py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" aria-current="page"><ion-icon class="text-xl px-2 -mb-1" src="{{asset('assets/images/icon/home.svg')}}"></ion-icon>Home</a>
